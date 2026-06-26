6c884f5cdc6682def1dfd386f553840b8530eda7
Petit Jump <killianbonnin7@gmail.com>
2026-06-26 14:58:12 +0200

    Modif du dico en compréention

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Les cases vide sont représenter par 0, celle du joueur 1 par un 1, et celle du joueur 2 par un 2.
 class Jeu:
     def __init__(self):
-        self.plateau =  {f"niveau {i}" : {f"ligne {i}" : [0 for i in range(6)] for i in range(6)} for i in range(6)}
+        self.plateau =  {f"niveau {i}" : {f"ligne {i}" : [0 for _ in range(6)] for i in range(6)} for i in range(6)}
         self.tours = 0
     
     def ajouter_pion(self):
