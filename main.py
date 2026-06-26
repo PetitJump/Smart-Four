d4b92d59577060da020cc2b860582e4f2738fb58
Petit Jump <killianbonnin7@gmail.com>
2026-06-26 14:57:26 +0200

    Ajout du dico en compréention

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+
 # Les cases vide sont représenter par 0, celle du joueur 1 par un 1, et celle du joueur 2 par un 2.
 class Jeu:
     def __init__(self):
-        self.plateau = {}
+        self.plateau =  {f"niveau {i}" : {f"ligne {i}" : [0 for i in range(6)] for i in range(6)} for i in range(6)}
         self.tours = 0
     
     def ajouter_pion(self):
