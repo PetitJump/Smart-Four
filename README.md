a08dbeaf68277d49f450579e0a59c6661a12cea2
PetitJump <killianbonnin7@gmail.com>
2026-06-28 12:30:25 +0200

    Ajout de la methode de base

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
+*Projet personel fait sans aucune aide d'ia.
 # En construction...
-
 # Réplique du jeu SMART FOUR de Giiker
-Projet personel fait sans aucune aide d'ia.
+
 
 ### But du jeu :
 ...
--- main.py
+++ main.py
@@ -3,19 +3,30 @@
 class Jeu:
     def __init__(self):
         self.plateau =  {f"niveau {i}" : {f"ligne {i}" : [0 for _ in range(6)] for i in range(6)} for i in range(6)}
-        self.tours = 0
+        self.tours, self.gagant = 0, 0
     
-    def ajouter_pion(self):
+    def ajouter_pion(self, x, y):
         pass
     
-    def afficher_plateau(self):
-        pass
 
-    def tour(self):
-        pass
+    def continuer(self):
+        return True
 
-    def start_game(self):
-        pass
+    def afficher_gagant(self):
+        print("Fonction en construction")
+
+
+    def game(self):
+        i = 1
+        while self.continuer():
+            i += 1
+            print(f"Joueur {i % 2 + 1} veuillez choisir une coordonée")
+            x = int(input("x = "))
+            y = int(input("y = "))
+            print("\n\n\n")
+            self.ajouter_pion(x, y)
+        self.afficher_gagant()
+        
 
 partie = Jeu()
-partie.start_game()
+partie.game()
