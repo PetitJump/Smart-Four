2a8773c3453422062e7d4f42f0f424ec9e3ad5fa
PetitJump <killianbonnin7@gmail.com>
2026-06-28 13:38:53 +0200

    Resolution du bug de le vérification horizontal

--- main.py
+++ main.py
@@ -2,23 +2,21 @@
 # Les cases vide sont représenter par 0, celle du joueur 1 par un 1, et celle du joueur 2 par un 2.
 class Jeu:
     def __init__(self):
-        self.plateau: dict =  {f"niveau {i}" : {f"ligne {i}" : [0 for _ in range(6)] for i in range(6)} for i in range(6)}
+        self.plateau: dict =  {f"niveau {i}" : {f"ligne {i}" : [0 for _ in range(6)] for i in range(1, 7)} for i in range(6)}
         self.tours: int = 0
         self.gagant: int = 0
     
     def ajouter_pion(self, x: int, y: int, joueur: int) -> None:
         """Ajoute un pion au plateau en fonctions des pions déjà en place"""
         for k in self.plateau:
-            if self.plateau[k][f"ligne {y}"][x + 1] == 0:
-                self.plateau[k][f"ligne {y}"][x + 1] = joueur
+            if self.plateau[k][f"ligne {y}"][x - 1] == 0:
+                self.plateau[k][f"ligne {y}"][x - 1] = joueur
                 break
 
     def continuer(self) -> bool:
         """Renvoie True si il n'y a aucun gagnant, sinon False"""
-        
         for pion in range(1, 3):
             for nv in self.plateau: # Parcour par niveau
-
                 ### Vérification horizontal ###
                 for y in self.plateau[nv]: # Parcour par ligne
                     liste: list = self.plateau[nv][y]
